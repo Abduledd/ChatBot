2343b27cf464cccfc22a709a0665e8a62b66b675
Abdul <abdou159159159@gmail.com>
2023-08-30 23:07:42 +0100

    server done and the response is also done

--- server/server.js
+++ server/server.js
@@ -1,63 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const openai = require('openai');
 const cors = require('cors');
-const fetch = require('node-fetch').default;
+const OpenAI = require('openai');
+
+const OPENAI_API_KEY = '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';
+
 
 const app = express();
 
-app.use(bodyParser.json());
+const openai = new OpenAI({
+    apiKey: OPENAI_API_KEY
+});
 
+app.use(bodyParser.json());
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
 
 
-const OPENAI_API_KEY = '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';
-openai.api_key = OPENAI_API_KEY;
-
 async function generateResponse(prompt) {
-    const apiKey = OPENAI_API_KEY;
-    const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-002/completions';
-
-    const requestData = {
+    const response = await openai.completions.create({
+        model: 'text-davinci-002',
         prompt: prompt,
         max_tokens: 1024,
-        n: 1,
-        stop: null,
-        temperature: 0.5
-    };
-
-    const response = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${apiKey}`
-        },
-        body: JSON.stringify(requestData)
+        temperature: 1,
     });
-
-    const data = await response.json();
-    const message = data.choices[0].text;
-
+    const message = response.choices[0].text;
     return message;
 }
-
 
 app.post('/api/sendMessage', async (req, res) => {
     const userInput = req.body.message;
     try {
-        // const completions = await openai.Completion.create({
-        //     engine: 'text-davinci-002',
-        //     prompt: userInput,
-        //     max_tokens: 1024,
-        //     n: 1,
-        //     stop: null,
-        //     temperature: 0.5,
-        // });
-        // const botReply = "Ok this is the response"
-
-        // const botReply = completions.choices[0].text;
         const botReply = await generateResponse(userInput);
         res.json({ message: botReply });
     } catch (error) {
--- server/serveur.js
+++ server/serveur.js
@@ -1,23 +1,19 @@
-const { Configuration, OpenAIApi } = require("openai");
+const OpenAI = require('openai');
 
-const config = new Configuration({
-    apiKey: "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"
+const openai = new OpenAI({
+    apiKey: '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'
 });
-
-const openai = new OpenAIApi(config);
-
 
 const runPrompt = async () => {
     const prompt = "Hello";
 
-    const response = await openai.createCompletion({
+    const response = await openai.completions.create({
         model: "text-davinci-002",
         prompt: prompt,
         max_tokens: 2048,
         temperature: 1,
     });
-
-    console.log(response.data);
+    console.log(response.choices[0].text);
 }
 
 runPrompt();
